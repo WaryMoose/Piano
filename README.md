d3423f389d8f4a96cb02dd6777479856ef1af6cc
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-01-05 11:42:51 +0545

    Key-Enabled

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Piano
+# Prashant's Mini Piano
+
+A touch enabled mobile friendly piano for easy playing...
+
+
---
+++ js/app.js
@@ -0,0 +1,219 @@
+window.addEventListener("load", function() {
+    //Initialize variables and the Howl library
+    var sound = new Howl({});
+    
+    //Listens for keypresses within the window and calls the keyClicked function
+    window.addEventListener("click", keyClicked);
+    //Listens for any touch event on the piano keys and calls the keyClicked function
+    document.getElementById("piano").addEventListener("touchstart", keyClicked);
+
+    document.addEventListener("keydown", function(ev){
+        var codeKey = ev.which || ev.keyCode;
+        var key = String.fromCharCode(codeKey);
+
+    //KeyPress-PlayNote function for each individual note
+        if (key == "A") {
+            notePressed("note1s");
+        }
+        if (key == "Q") {
+            notePressed("note2s");
+        }
+        if (key == "S") {
+            notePressed("note3s");
+        }
+        if (key == "W") {
+            notePressed("note4s");
+        }
+        if (key == "D") {
+            notePressed("note5s");
+        }
+        if (key == "F") {
+            notePressed("note6s");
+        }
+        if (key == "E") {
+            notePressed("note7s");
+        }
+        if (key == "G") {
+            notePressed("note8s");
+        }
+        if (key == "R") {
+            notePressed("note9s");
+        }
+        if (key == "X") {
+            notePressed("note10s");
+        }
+        if (key == "T") {
+            notePressed("note11s");
+        }
+        if (key == "C") {
+            notePressed("note12s");
+        }
+        if (key == "V") {
+            notePressed("note1o");
+        }
+        if (key == "Y") {
+            notePressed("note2o");
+        }
+        if (key == "B") {
+            notePressed("note3o");
+        }
+        if (key == "U") {
+            notePressed("note4o");
+        }
+        if (key == "N") {
+            notePressed("note5o");
+        }
+        if (key == "H") {
+            notePressed("note6o");
+        }
+        if (key == "I") {
+            notePressed("note7o");
+        }
+        if (key == "J") {
+            notePressed("note8o");
+        }
+        if (key == "O") {
+            notePressed("note9o");
+        }
+        if (key == "K") {
+            notePressed("note10o");
+        }
+        if (key == "P") {
+            notePressed("note11o");
+        }
+        if (key == "L") {
+            notePressed("note12o");
+        }
+    });
+    
+    //keyClicked function which handles the key clicked on the keyboard and passes the respective notes to other functions
+    function keyClicked(event) {
+        //For test purpose
+        //console.log("Clicked note : " + event.target.id);
+        
+        //Touch or click check function for each individual note
+        if (event.target.id == "note1s") {
+            notePressed("note1s");
+        }
+        if (event.target.id == "note2s") {
+            notePressed("note2s");
+        }
+        if (event.target.id == "note3s") {
+            notePressed("note3s");
+        }
+        if (event.target.id == "note4s") {
+            notePressed("note4s");
+        }
+        if (event.target.id == "note5s") {
+            notePressed("note5s");
+        }
+        if (event.target.id == "note6s") {
+            notePressed("note6s");
+        }
+        if (event.target.id == "note7s") {
+            notePressed("note7s");
+        }
+        if (event.target.id == "note8s") {
+            notePressed("note8s");
+        }
+        if (event.target.id == "note9s") {
+            notePressed("note9s");
+        }
+        if (event.target.id == "note10s") {
+            notePressed("note10s");
+        }
+        if (event.target.id == "note11s") {
+            notePressed("note11s");
+        }
+        if (event.target.id == "note12s") {
+            notePressed("note12s");
+        }
+        if (event.target.id == "note1o") {
+            notePressed("note1o");
+        }
+        if (event.target.id == "note2o") {
+            notePressed("note2o");
+        }
+        if (event.target.id == "note3o") {
+            notePressed("note3o");
+        }
+        if (event.target.id == "note4o") {
+            notePressed("note4o");
+        }
+        if (event.target.id == "note5o") {
+            notePressed("note5o");
+        }
+        if (event.target.id == "note6o") {
+            notePressed("note6o");
+        }
+        if (event.target.id == "note7o") {
+            notePressed("note7o");
+        }
+        if (event.target.id == "note8o") {
+            notePressed("note8o");
+        }
+        if (event.target.id == "note9o") {
+            notePressed("note9o");
+        }
+        if (event.target.id == "note10o") {
+            notePressed("note10o");
+        }
+        if (event.target.id == "note11o") {
+            notePressed("note11o");
+        }
+        if (event.target.id == "note12o") {
+            notePressed("note12o");
+        }
+        
+        //Clicked Space for rest
+        //if (event.keyCode == 32) {
+        //    event.preventDefault();
+        //    notePressed("space");
+        //}
+        
+        event.preventDefault();
+        return false;
+    }
+    
+    //notePressed function to identify if its a real note or a pause i.e.Space
+    function notePressed(note) {
+        //if its not a space then call the playNote function
+        if (note != "space") {
+            playNote(note);
+        } else {
+            //Reset the design of the keyboard notes
+            var mks = document.querySelectorAll(".major-key");
+            for (var i=0; i<mks.length; i++) {
+                mks[i].style.backgroundColor = "#fff";   
+            }
+            var sks = document.querySelectorAll(".sharp-Key");
+            for (var i=0; i<sks.length; i++) {
+                sks[i].style.backgroundColor = "#222";   
+            }
+            //Stop the playing sounds
+            sound.stop();
+        }
+    }   
+    
+    //playNote function
+    function playNote(note) {
+        sound = new Howl({
+            urls: ["audio/"+note+".mp3"],
+            onplay: function() {
+                //Make the respective note appear like its clicked
+                document.querySelector("#"+note).style.backgroundColor = "#bdc3c7";
+            },
+            onend: function() {
+                //Reset the respective note to appear like its not clicked
+                if (note == "note2s" || note == "note2o" || note == "note4s" || note == "note4o" || 
+                    note == "note7s" || note == "note7o" || note == "note9s" || note == "note9o" ||
+                    note == "note11s" || note == "note11o") {
+                    document.querySelector("#"+note).style.backgroundColor = "#222";
+                } else {
+                    document.querySelector("#"+note).style.backgroundColor = "#fff";
+                }
+            }
+        }).play();
+    }
+    
+});
