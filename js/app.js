806ea306dbfaca323ac61912dcc2d20e20f42bb3
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-01-07 14:51:34 +0545

    Update app.js

--- js/app.js
+++ js/app.js
@@ -201,7 +201,7 @@ window.addEventListener("load", function() {
             urls: ["audio/"+note+".mp3"],
             onplay: function() {
                 //Make the respective note appear like its clicked
-                document.querySelector("#"+note).style.backgroundColor.linear-gradient = "to top, black, white";
+                document.querySelector("#"+note).style.backgroundColor.createLinearGradient("90deg, #aaa, #666");
             },
             onend: function() {
                 //Reset the respective note to appear like its not clicked
